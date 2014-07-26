1b3f8a1df6bb2be606851e9526f518758606fa69
Matt Hoffman <lord.matt.hoffman@gmail.com>
2014-07-25 23:41:37 -0700

    Next 16 Z80 commands.

--- Emulator/GameBoyEmu/Z80.cpp
+++ Emulator/GameBoyEmu/Z80.cpp
@@ -132,6 +132,121 @@ void Z80::ExecuteInstruction(char instruction)
 		flags = (flags & 0xEF)+co;
 		regM = 1;
 		break;
+		/* Stop Processor */
+	case 0x10:
+		printf("Stop Processor");
+		break;
+		/* Load 16-bit immediate into DE */
+	case 0x11:
+		regE = pMem->Read8(regPc);
+		regD = pMem->Read8(regPc+1);
+		regPc +=2;
+		regM = 3;
+		break;
+		/* Save A to address pointed to by DE */
+	case 0x12:
+		pMem->Write8(regA, (regD << 8) + regE);
+		regM = 2;
+		break;
+		/* Increment 16-bit DE */
+	case 0x13:
+		regE = (regE+1) & 0xFF;
+		if(!regE)
+			regD = (regD+1) & 0xFF;
+		regM = 1;
+		break;
+		/* Increment D */
+	case 0x14:
+		regD++;
+		regD &= 0xFF;
+		flags = regD ? 0 : Zero;
+		regM = 1;
+		break;
+		/* Decrement D */
+	case 0x15:
+		regD--;
+		regD &= 0xFF;
+		flags = regD ? 0 : Zero;
+		regM = 1;
+		break;
+		/* Load 8-bit immediate into D */
+	case 0x16:
+		regD = pMem->Read8(regPc);
+		regPc++;
+		regM = 2;
+		break;
+		/* Rotate A left */
+	case 0x17:
+		int ci = flags & Carry ? 1 : 0;
+		int co = regA & Zero ? Carry : 0;
+		regA = (regA << 1) + ci; 
+		regA &= 0xFF;
+		flags = (flags & 0xEF)+co;
+		regM = 1;
+		break;
+		/* Relative jump by signed immediate */
+	case 0x18:
+		char i = pMem->Read8(regPc);
+		if(i > 127)
+			i =-((~i+1)&0xFF);
+		regPc++;
+		regM = 2;
+		regPc += i;
+		regM++;
+		break;
+		/* Add 16-bit DE to HL */
+	case 0x19:
+		int hl = (regH << 8) + regL;
+		hl += (regD <<8)+regE;
+		if(hl > 0xFFFF)
+			flags |= Carry;
+		else
+			flags |= 0xEF;
+		regH = (hl >> 8) & 0xFF;
+		regL = hl&0xFF;
+		regM = 3;
+		break;
+		/* Load A from address pointed to by DE */
+	case 0x1A:
+		regA = pMem->Read8((regD << 8) + regE);
+		regM = 2;
+		break;
+		/* Decrement 16-bit DE */
+	case 0x1B:
+		regE = (regE-1)&0xFF;
+		if(regE == 0xFF)
+			regD = (regD-1)&0xFF;
+		regM = 1;
+		break;
+		/* Increment E */
+	case 0x1C:
+		regE++;
+		regE &= 0xFF;
+		flags = regC ? 0 : Zero;
+		regM = 1;
+		break;
+		/* Decrement E */
+	case 0x1D:
+		regE--;
+		regE &= 0xFF;
+		flags = regE ? 0 : Zero;
+		regM = 1;
+		break;
+		/* Load 8-bit immediate into E */
+	case 0x1E:
+		regE = pMem->Read8(regPc);
+		regPc++;
+		regM = 2;
+		break;
+		/* Rotate A right */
+	case 0x1F:
+		int ci = flags & Carry ? Zero : 0;
+		int co = regA & 1 ? Carry : 0;
+		regA = (regA >> 1) + ci;
+		regA &= 0xFF;
+		flags = (flags & 0xEF) + co;
+		regM = 1;
+		break;
 	}
 
 }
